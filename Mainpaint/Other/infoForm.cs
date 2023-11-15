a65460511553a8e322e9f44189dee7c3f5391a71
paulino <115406383+paulino237@users.noreply.github.com>
2023-11-15 20:32:55 +0100

    Projet

---
+++ Mainpaint/Other/infoForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Mainpaint.Other
+{
+    public partial class infoForm : Form
+    {
+        public infoForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
